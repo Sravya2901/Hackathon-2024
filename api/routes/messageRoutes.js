f86796ab391b3f510ae0f09c8ec10ef8ffe8e7cc
Sravya <71391139+Sravya2901@users.noreply.github.com>
2024-05-21 13:08:55 +0530

    Update messageRoutes.js

--- api/routes/messageRoutes.js
+++ api/routes/messageRoutes.js
@@ -2,7 +2,7 @@
 import express from 'express';
 const router = express.Router();
 // const { sendMessage } = require('../controllers/messageController');
-import {sendMessage} from '../controllers/messageController.js';
+import sendMessage from '../controllers/messageController.js';
 
 // POST /send
 router.post('/send', sendMessage);
