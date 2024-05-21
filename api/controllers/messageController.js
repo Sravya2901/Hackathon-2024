15e47d25a8ca122cd32fd5d49b2efd61c6bde595
Sravya <71391139+Sravya2901@users.noreply.github.com>
2024-05-21 13:09:49 +0530

    Update messageController.js

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -1,29 +1,26 @@
-//uncomment this code and run
-// import 'dotenv/config';
-// import twilio from 'twilio';
+// const twilio = require('twilio');
+import twilio from "twilio";
+const accountSid = 'ACb267057d33c06ee6438a3829dd255cfa'; // Your Account SID from www.twilio.com/console
+const authToken = '3cabde909996cd8136fdaa5ffbded3e7';   // Your Auth Token from www.twilio.com/console
+const twilioPhoneNumber = '+16264869770';
 
-// // Now, you can directly access process.env
-// const accountSid = '';
-// const authToken = '';  
-// const twilioPhoneNumber = '+16264869770';
-// // Define sendMessage outside, so it's properly scoped and can be exported
-// export async function sendMessage(to, body) {
-//   const client = twilio(accountSid, authToken);
-//   try {
-//     const message = await client.messages.create({
-//       body,
-//       to, // Assuming 'to' is a parameter of sendMessage
-//       from: twilioPhoneNumber
-//     });
-//     console.log('Message sent:', message.sid);
-//     // Assuming you want to return this for chaining or await
-//     return { success: true, messageSid: message.sid };
-//   } catch (error) {
-//     console.error('Error sending message:', error);
-//     return { success: false, error: error.message };
-//   }
-// }
+const client = twilio(accountSid, authToken);
 
-// // If you're using this in an Express app, you'd likely want to wrap this in another function
-// // that handles the req, res objects, rather than exporting directly.
-// // export default { sendMessage };
+const sendMessage = async (req, res) => {
+  const { to, body } = req.body;
+  console.log(to)
+  try {
+    const message = await client.messages.create({
+      body: body,
+      to: '+917801049826',
+      from: twilioPhoneNumber
+    });
+
+    res.status(200).json({ success: true, messageSid: message.sid });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+export default sendMessage;
