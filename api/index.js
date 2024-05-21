b5be6948028ee615bca5e74e2f141f679c7d14da
Sravya <71391139+Sravya2901@users.noreply.github.com>
2024-05-21 13:10:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 // const express = require('express');
-// Now process.env.TWILIO_ACCOUNT_SID will be available
 import express from 'express';
 // const bodyParser = require('body-parser');
 import bodyParser from 'body-parser';
@@ -8,13 +7,12 @@ import messageRoutes from './routes/messageRoutes.js'
 const app = express();
 // const cors = require('cors');
 import cors from 'cors';
-    const port=5000;
-    // The rest of your application logic here, or import the rest of your application modules
 app.use(cors());
-console.log(port)
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use('/', messageRoutes);
 app.listen(port, () => {
 
     console.log(`Server is running on port ${port}`);
-})
+
+});
